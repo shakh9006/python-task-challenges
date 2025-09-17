c8c453f420cae23bd35ced321cedaff64ca600fb
swift <waxboz.nematov@gmail.com>
2025-09-17 12:34:26 +0500

    3Sum

---
+++ leetcode/3-sum.py
@@ -0,0 +1,40 @@
+# Source: https://leetcode.com/problems/3sum/description/
+
+# Title: 3Sum
+
+# Description
+
+"""
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k,
+and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+"""
+
+
+def three_sum(nums):
+    res = []
+    nums.sort()
+
+    for i in range(len(nums)):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+
+        j = i + 1
+        k = len(nums) - 1
+
+        while j < k:
+            total = nums[i] + nums[j] + nums[k]
+
+            if total > 0:
+                k -= 1
+            elif total < 0:
+                j += 1
+            else:
+                res.append([nums[i], nums[j], nums[k]])
+                j += 1
+
+                while nums[j] == nums[j - 1] and j < k:
+                    j += 1
+
+    return res
